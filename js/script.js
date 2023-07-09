53733cb8dfa0b5a624d027bb425355ab63515926
weiowyw <haxjlebatop@gmail.com>
2023-07-09 21:06:55 +0500

    dasdasdas

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+// 1.
+// const arr = [3, 4, 6, 10, "future"];
+//
+// arr.forEach(item => console.log(item));
+
+// 1.1.
+// const arr = [3, 4, 6, 10, "future"];
+//
+// arr.forEach(item => item > 4 ? console.log(item) : false);
+
+
+// 2.
+// const arr = ["I", "would", "like", "to", "become", "the one"];
+// const elem = "the one";
+//
+// const findBoolean = (array, element) => array.includes(element);
+//
+// console.log(findBoolean(arr, elem));
+
+
+// 3.
+// const arr = ['user', 'user1', 'user10', 'user100', 'user1000'];
+//
+// const callback = (item) => item.length > 4;
+// console.log(arr.find(callback));
+
+
+// 4.
+// const arr = ['user', 'user1', 'user10', 'user100', 'user1000'];
+//
+// const result = arr.filter(item => item.length > 5);
+//
+// console.log(result);
